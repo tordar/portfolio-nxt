aff8162ba405921fb14db0241cbe191e5d35e27c
tordar <tordar.tommervik@gmail.com>
2021-09-29 17:06:38 +0200

    Link tag added

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -22,10 +23,11 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="/projects" className={styles.card}>
+          <Link href="/projects"><a className={styles.card}>
             <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
+          </Link>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h2>Learn &rarr;</h2>
