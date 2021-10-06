7b4e836baf89a3184a10e0001e39771b4a72cb28
tordar <tordar.tommervik@gmail.com>
2021-10-06 12:58:39 +0200

    updated card component

--- components/Card.js
+++ components/Card.js
@@ -9,12 +9,20 @@ const Card = (props) => {
 
     return (
         <div className={style.card}>
-                <div className="title">
-                    <h4>{props.title}</h4>
-                    <div>
-                    <p>{props.info}</p>
+                <div>
+                    <h4 className={style.title}>{props.title}</h4>
+                    <p className={style.info}>{props.info}</p>
                     {/* <div className={style.icon}>{element}</div> */}
                 </div>
+                <div className={style.iconContainer}>
+                    <div className={style.icons}>
+                        <div>hello</div>
+                        <div>hello</div>
+                    </div>
+                    <div className={style.techIcons}>
+                        <div>hello</div>
+                        <div>hello</div>
+                    </div>
                 </div>
             </div>
     )
